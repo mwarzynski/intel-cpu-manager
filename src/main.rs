d6e4de39b64b6249dffe4c1415a9554010b50670
Mateusz Warzyński <mateusz@warzynski.pro>
2024-11-01 18:50:16 +0100

    feat: print out current settings on no args

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn hwp_dynamic_boost_enable() -> Result<(), PStateError> {
     let _ = pstate.set_hwp_dynamic_boost(true);
     pstate.set_min_perf_pct(0)?;
     pstate.set_max_perf_pct(100)?;
-    pstate.set_no_turbo(true)?;
+    pstate.set_no_turbo(false)?;
     Ok(())
 }
 
@@ -25,6 +25,25 @@ fn turbo_disable() -> Result<(), PStateError> {
     pstate.set_min_perf_pct(0)?;
     pstate.set_max_perf_pct(50)?;
     pstate.set_no_turbo(true)?;
+    Ok(())
+}
+
+fn print_info() -> Result<(), PStateError> {
+    let pstate = PState::new()?;
+    let values = pstate.values()?;
+
+    let turbo_enabled = !values.no_turbo;
+
+    println!("turbo: \x1b[33m{}\x1b[0m", turbo_enabled);
+    println!(
+        "hwp_dynamic_boost: \x1b[33m{:?}\x1b[0m",
+        values.hwp_dynamic_boost
+    );
+    println!(
+        "perf_pct_range: \x1b[33m{}-{}\x1b[0m",
+        values.min_perf_pct, values.max_perf_pct,
+    );
+
     Ok(())
 }
 
@@ -41,6 +60,8 @@ fn main() -> Result<(), PStateError> {
     } else if should_enable_auto {
         println!("\x1b[33mAuto selection mode!\x1b[0m");
         hwp_dynamic_boost_enable()?;
+    } else if args.len() == 1 {
+        print_info()?;
     } else {
         println!("\x1b[32mDisable TURBO.\x1b[0m");
         turbo_disable()?;
